0db6f0d30c8bae3f899e840f141c4eb9c40d5870
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-13 14:54:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const form = document.querySelector('form');
+const ageInput = document.querySelector('#age');
+const nameInput = document.querySelector('#name');
+const submitBtn = document.querySelector('#btn');
+
+submitBtn.addEventListener('click', event => {
+  event.preventDefault(); // Prevent default form submission
+
+  const age = ageInput.value;
+  const name = nameInput.value;
+
+  if (!age || !name) {
+    alert('Please fill out all fields');
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000); // Delay of 4 seconds
+  });
+
+  promise
+    .then(message => alert(message))
+    .catch(errorMessage => alert(errorMessage));
+});
+
+
